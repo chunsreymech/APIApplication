4b4a6bb4b4fec9fd99128aacac43d61e19d293f4
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-07-15 20:33:45 +0700

    Update StudentController.cs

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -11,19 +11,25 @@ namespace APIApplication.Controllers
         // បញ្ជីសិស្សសិប្បនិម្មិតក្នុង memory
         private static List<Student> _students = new()
         {
-            new Student { Id = 1, Name = "Dara SOK", Major = "Computer Science" },
-            new Student { Id = 2, Name = "Cheata SENG", Major = "Information Technology" },
-            new Student { Id = 3, Name = "Sopheap CHHAY", Major = "Software Engineering" },
-            new Student { Id = 4, Name = "Rithy KIM", Major = "Data Science" },
-            new Student { Id = 5, Name = "Sreyneang HENG", Major = "Cybersecurity" }
+            new Student { Id = 1, Name = "Dara SOK", Major = "Computer Science", DOB = new DateTime(2000, 1, 1), Email = "dara.sok@example.com", Telegram = "@dara_sok" },
+            new Student { Id = 2, Name = "Cheata SENG", Major = "Information Technology", DOB = new DateTime(2001, 2, 2), Email = "cheata.seng@example.com", Telegram = "@cheata_seng" },
+            new Student { Id = 3, Name = "Sopheap CHHAY", Major = "Software Engineering", DOB = new DateTime(2002, 3, 3), Email = "sopheap.chhay@example.com", Telegram = "@sopheap_chhay" },
+            new Student { Id = 4, Name = "Rithy KIM", Major = "Data Science", DOB = new DateTime(2003, 4, 4), Email = "rithy.kim@example.com", Telegram = "@rithy_kim" },
+            new Student { Id = 5, Name = "Sreyneang HENG", Major = "Cybersecurity", DOB = new DateTime(2004, 5, 5), Email = "sreyneang.heng@example.com", Telegram = "@sreyneang_heng" }
         };
 
         // ១. GET: api/student (ទាញយកសិស្សទាំងអស់)
         [HttpGet]
-        public IActionResult GetAll() => Ok(_students);
+        //public IActionResult GetAll() => Ok(_students);
+        public IActionResult GetAll()
+        {
+            if (_students.Count == 0)
+                return NotFound("មិនមានសិស្សនៅក្នុងបញ្ជីទេ");
+            return Ok(_students);
+        }
 
         // ២. GET: api/student/1 (ទាញយកតាម ID)
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] //Route Attribute and Parameter Binding
         public IActionResult GetById(int id)
         {
             var student = _students.Find(X => X.Id == id);
