4c05aba4719c0ccc5dadb1fcf8f981b3c1fc79cc
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-07-15 20:26:25 +0700

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIApplication.Models
-{
+{    
     public class Student
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Major { get; set; } = string.Empty;
     }
+
 }
